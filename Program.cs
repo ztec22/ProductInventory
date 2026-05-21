383fa618cc415fefdf718e718c6dec4bf9fd3902
ztec22 <257952406+ztec22@users.noreply.github.com>
2026-05-21 14:52:17 +0200

    add openapi / swagger

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
 
 app.UseHttpsRedirection();
