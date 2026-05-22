783820a90d33ce04db3a5d35bc3a2f1d7f541f81
ztec22 <257952406+ztec22@users.noreply.github.com>
2026-05-22 09:12:06 +0200

    migration: add product data

---
+++ Migrations/20260522070847_AddProductData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProductInventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Amount", "Brand", "Category", "Name", "Price", "Vendor" },
+                values: new object[,]
+                {
+                    { 1, 1, "Brand 1", "Electronics", "Mouse", 6.5, "Vendor 1" },
+                    { 2, 1, "Brand 2", "Electronics", "Keyboard", 9.5, "Vendor 2" },
+                    { 3, 1, "Brand 3", "Electronics", "Screen", 120.59999999999999, "Vendor 3" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- Models/ProductContext.cs
+++ Models/ProductContext.cs
@@ -9,5 +9,41 @@ public class ProductContext : DbContext
     {
     }
 
+    private List<Product> products = [
+        new Product {
+            Id = 1,
+            Name = "Mouse",
+            Category = "Electronics",
+            Brand = "Brand 1",
+            Vendor = "Vendor 1",
+            Price = 6.5,
+            Amount = 1,
+        },
+        new Product {
+            Id = 2,
+            Name = "Keyboard",
+            Category = "Electronics",
+            Brand = "Brand 2",
+            Vendor = "Vendor 2",
+            Price = 9.5,
+            Amount = 1,
+        },
+        new Product {
+            Id = 3,
+            Name = "Screen",
+            Category = "Electronics",
+            Brand = "Brand 3",
+            Vendor = "Vendor 3",
+            Price = 120.6,
+            Amount = 1,
+        },
+    ];
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(products);
+    }
+    
     public DbSet<Product> Products { get; set; } = null!;
 }
